0f6888146716b8d0b878ba4e4cbc61227b259ea0
RaviKiran25052 <ravivarma25052@gmail.com>
2025-06-01 16:07:48 +0530

    loading

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -42,8 +42,6 @@ const Footer = () => {
 
 		try {
 			setLoading(true);
-			console.log(API_URL);
-
 			const res = await axios.post(`${API_URL}/contact`, formData);
 			if (res.status === 201) {
 				toast.success(res.data.message);
@@ -179,11 +177,11 @@ const Footer = () => {
 								<button
 									onClick={handleSubmit}
 									disabled={!formData.name || !formData.mobile || !formData.email || !formData.service}
-									className="px-8 py-3 bg-transparent border border-white text-white hover:bg-white hover:text-gray-900 rounded-full font-medium transition-colors duration-200 group"
+									className="px-8 py-3 bg-transparent border border-white text-white group hover:bg-white hover:text-gray-900 rounded-full font-medium transition-colors duration-200 group"
 								>
 									{loading ? (
 										<span className="inline-flex items-center justify-center ">
-											<svg className="animate-spin h-5 w-5 mr-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+											<svg className="animate-spin h-5 w-5 mr-3 group-hover:text-gray-900 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 												<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
 												<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2.93 6.343A8.001 8.001 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3.93-1.595z"></path>
 											</svg>
